5481b933aeaa307f82649c46c859a59fedcbdab4
Carl Handy <carl@kalpa.dev>
2024-10-25 23:58:52 -0400

    add tls

---
+++ internal/server/security/tls.go
@@ -0,0 +1,24 @@
+package security
+
+import (
+    "crypto/tls"
+)
+
+func SecureTLSConfig() *tls.Config {
+    return &tls.Config{
+        MinVersion: tls.VersionTLS12,
+        CipherSuites: []uint16{
+            tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+            tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+            tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+            tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+            tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+            tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+        },
+        PreferServerCipherSuites: true,
+        CurvePreferences: []tls.CurveID{
+            tls.X25519,
+            tls.CurveP256,
+        },
+    }
+}
