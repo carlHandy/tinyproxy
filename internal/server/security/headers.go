751fda880b452d606617c0755ea76900d35e14be
Carl Handy <carl@kalpa.dev>
2024-10-25 23:56:57 -0400

    add sec headers

--- internal/server/security/headers.go
+++ internal/server/security/headers.go
@@ -1,5 +1,9 @@
 package security
 
+import (
+    "net/http"
+)
+
 func SecurityHeaders(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("X-Frame-Options", "SAMEORIGIN")
